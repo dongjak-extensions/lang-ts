a787f5177d9e118155ff153e46c5740d1b3f3c28
cruldra <cruldra@gmail.com>
2023-11-26 20:24:38 +0800

    feat: 添加jest
    BREAKING CHANGE: 11

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -1,7 +1,7 @@
-
-
 import '../index'
+
 test('simpleFunction returns correct value', async () => {
-    const a = [1,23,4]
-console.log(  a.getSingleOrArray$ext())
+    const a = [1, 23, 4]
+    console.log(a.getSingleOrArray$ext())
+    console.log(11)
 });
