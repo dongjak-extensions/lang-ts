313905406a95953ada8f90ba09fdec3acde6559d
cruld <cruldra@gmail.com>
2024-01-29 22:21:01 +0800

    🐛 解决更新时不能正常计算差异对象的问题

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ export * from './src/ArrayUtil';
 export * from './src/RandomUtil';
 export * from './src/MapUtil';
 export * from './src/RegexUtil';
+export * from './src/ObjectUtils';
 if (!Array.prototype.groupBy$ext) {
     Array.prototype.groupBy$ext = function <T, K extends keyof T>(this: T[], key: K): Map<T[K], T[]> {
         return ArrayUtil.groupBy(this, key)
