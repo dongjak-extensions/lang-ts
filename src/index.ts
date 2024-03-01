bb60560a578a977f423b55deea19cd7f3401ebc0
cruld <cruldra@gmail.com>
2024-03-01 15:27:10 +0800

    ✨ 添加公共类型

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export {default as SortDirectionUtil} from "./utils/SortDirectionUtil";
+
 export {default as ArrayUtil} from './utils/ArrayUtil';
 export {default as RandomUtil} from './utils/RandomUtil';
 export {default as MapUtil, type EnumMap} from './utils/MapUtil';
@@ -5,3 +7,15 @@ export {default as RegexUtil} from './utils/RegexUtil';
 export {default as ObjectUtils} from './utils/ObjectUtils';
 export {default as StreamUtils} from './utils/StreamUtils';
 export {default as BufferUtils} from './utils/BufferUtils'
+
+
+export * from './web/JsonResponse'
+export {type IFilter} from './data/IFilter';
+export {type IPage} from './data/IPage';
+export {type IQueryPayload} from './data/IQueryPayload';
+export * from './data/ISort';
+export * from './data/Operator';
+export * from './data/Operators';
+export * from './data/Pages';
+export * from './data/QueryPayloads';
+export * from './data/SortDirection';
---
+++ src/utils/SortDirectionUtil.ts
@@ -0,0 +1,14 @@
+import {SortDirection} from "../data/SortDirection";
+
+export default class SortDirectionUtil {
+    static valueOf(value: string): SortDirection {
+        switch (value.toUpperCase()) {
+            case SortDirection.ASC.toUpperCase():
+                return SortDirection.ASC;
+            case SortDirection.DESC.toUpperCase():
+                return SortDirection.DESC;
+            default:
+                throw new Error(`不支持的排序方向:${value}`)
+        }
+    }
+}
